39aeb5da63a9680fafc9fc2c1b0a1ebf775327db
Daniel Fimiani <danielfimiani@outlook.com>
2020-10-21 23:58:53 -0300

    Quedo funcionando

--- app.js
+++ app.js
@@ -3,26 +3,29 @@
 // requires
 const express = require("express");
 const bodyparser = require("body-parser");
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 require("dotenv").config();
 
 // ejecutar express
-const indexRouter = require('./routes/indexRoutes');
 const app = express();
 
-// CARGAR FICHEROS DE RUTAS
+//fichero de rutas
+const indexRouter = require("./routes/indexRoutes");
 
 // Middlewares
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-//Sets our app to use the handlebars engine
-app.set('view engine', 'handlebars');
-//Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
-  layoutsDir: __dirname + '/views/layouts',
-}));
-app.use('/', indexRouter);
-// rescribir rutas
-// Exportar modulo
-module.exports = app;
+//Render Engine
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use("/", indexRouter);
+
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`el servitdor se esta ejecutando en http://localhost:${port}`);
+});
---
+++ routes/indexRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+//HOME
+router.get("/", function (req, res) {
+  res.render("home");
+});
+
+//Login
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+//Registro
+router.get("/registro", function (req, res) {
+  res.render("registro");
+});
+
+
+//ADMIN PRODUCTOS
+router.get("/admin/productos", function (req, res) {
+  res.render("productos");
+});
+
+router.get("/admin/compras", function (req, res) {
+  res.render("compras");
+});
+
+
+module.exports = router;
