b8c649d9bcacdbb6987fc1c87595f64bee500c9f
Daniel Fimiani <danielfimiani@outlook.com>
2020-10-21 21:44:11 -0300

    Agrego handlebars

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
 // requires
-const express = require('express')
-const bodyparser = require('body-parser')
+const express = require("express");
+const bodyparser = require("body-parser");
+const exphbs = require("express-handlebars");
 require("dotenv").config();
-
 
 // ejecutar express
 const app = express();
@@ -12,10 +12,13 @@ const app = express();
 // CARGAR FICHEROS DE RUTAS
 
 // Middlewares
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", ".hbs");
 
 // rescribir rutas
 
-// Exportar modulo 
+// Exportar modulo
 module.exports = app;
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-'use strict'
+"use strict";
 
-const app = require('./app')
+const { render } = require("./app");
+const app = require("./app");
+
 require("dotenv").config();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
 app.listen(port, () => {
   console.log(`el servitdor se esta ejecutando en http://localhost:${port}`);
-})
+});
