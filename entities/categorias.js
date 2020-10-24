96da825d078c8989a7d34811438d287beec9622e
FRANCO <francoestrella55@gmail.com>
2020-10-24 17:46:28 -0300

    agregado las entidades estaticas para el e-commers

---
+++ entities/categorias.js
@@ -0,0 +1,20 @@
+const categories = [
+  {
+    id_categoria: "1",
+    txt_desc: "herramientas",
+  },
+  {
+    id_categoria: "2",
+    txt_desc: "bazar",
+  },
+  {
+    id_categoria: "3",
+    txt_desc: "electronica",
+  },
+  {
+    id_categoria: "4",
+    txt_desc: "hogar",
+  },
+];
+
+module.exports = categories;
